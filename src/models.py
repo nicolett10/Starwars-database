047ccb33084fd89c5a3afd8fc2bd61f79413f129
nicolett10 <torrestapia.n@gmail.com>
2022-10-05 19:26:56 +0000

    starwars

--- src/models.py
+++ src/models.py
@@ -6,28 +6,44 @@ from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 from eralchemy import render_er
 
-Base = declarative_base()
+Base = declarative_base ()
+class User(Base):
+    __tablename__ = 'users'
+    id = Column (Integer, primary_key = True)
+    firstname = Column (String(30), nullable=False) 
+    lastname = Column (String(30), nullable=False) 
+    email = Column (String(50), nullable=False) 
+    password = Column (String(10), nullable=False) 
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class Favorite (Base):
+    __tablename__ = 'favorites'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    users_id = Column(Integer, ForeignKey('users.id'))
+    characters_id = Column(Integer, ForeignKey('characters.id'))
+    planets_id = Column(Integer, ForeignKey('planets.id'))
+    user = relationship('User')
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+class Planet(Base):
+    __tablename__ = 'planets'
 
-    def to_dict(self):
-        return {}
+    id = Column(Integer, primary_key = True)
+    name = Column (String(30), nullable=False)
+    climate = Column (String(50), nullable=False) 
+    diameter = Column (String(50), nullable=False) 
+    films = Column (String(50), nullable=False) 
+    gravity = Column (String(50), nullable=False) 
+    population = Column (String(50), nullable=False) 
+    residents = Column (String(50), nullable=False) 
+    terrain = Column (String(50), nullable=False) 
 
-## Draw from SQLAlchemy base
+class Character(Base):
+    __tablename__ = 'characters'
+    
+    id = Column(Integer, primary_key = True)
+    films = Column (String(50), nullable=False) 
+    name = Column (String(30), nullable=False) 
+    species = Column (String(50), nullable=False) 
+    starships = Column (String(50), nullable=False) 
+    role_by = Column (String(50), nullable=False) 
+
 render_er(Base, 'diagram.png')
